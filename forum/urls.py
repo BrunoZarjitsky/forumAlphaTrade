2f69efb13f3c87aec40dc279c41a851c01628b04
Bruno Zarjitsky <brunozar@poli.ufrj.br>
2020-10-19 15:15:48 -0300

    forum/urls.py configurado, siteForum/urls.py criado

--- forum/urls.py
+++ forum/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path("", include("siteForum.urls")),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_URL) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

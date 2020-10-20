9640b15c119810cca23cb791f19273f379caf7cf
Bruno Zarjitsky <brunozar@poli.ufrj.br>
2020-10-19 22:16:26 -0300

    base_page.html: 	arrumei o navbar. 	Preciso arrumar o footer index.html: 	Botei o card 	botei as tabelas das ultimas enquetes, e do em alta, e tambem o perfil 	Preciso complementar o perfil e arrumar os links dos botoes

--- siteForum/models.py
+++ siteForum/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+
+class tipo(models.Model):
+    tipoChoice = (
+        ("Python", "Python"),
+        ("Django", "Django"),
+        ("Html", "Html"),
+        ("CSS", "CSS"),
+        ("Linux", "Linux"),
+        ("Bootstrap", "Bootstrap"),
+        ("FontAwesome", "FontAwesome"),
+    )
+    tipo = models.CharField(max_length=50, choices=tipoChoice)
+
+    def __str__(self):
+        return self.tipo
+
+class enquete(models.Model):
+    titulo = models.CharField(max_length=200)
+    descricao = models.TextField(default="")
+    tipo = models.ManyToManyField(tipo, blank=True)
+
+    class Meta:
+        verbose_name = "Enquete"
+        verbose_name_plural = "Enquetes"
+    
+    def __str__ (self):
+        return self.titulo
--- siteForum/views.py
+++ siteForum/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from random import randint
+from .models import *
 
 # Create your views here.
 
 def Index(request):
+    enquetes = enquete.objects.all()
+    print(enquetes[0].tipo.all())
     return render(request, "index.html", {"background": "background"+str(randint(1,3))+".jpg"})
